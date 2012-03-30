6bda01bbe475debb4892f3f5b2af27752dff1cac
Mark Gunnels <mark@mark-Lemur-Ultra.(none)>
2012-03-30 08:07:56 -0400

    updated project.clj

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "Clojure library to make use of Amazon's Simple Workflow Framework"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [com.amazonaws/aws-java-sdk "1.3.3"]]
-  :plugins [[lein-swank "1.4.3"]])
+  :plugins [[lein-swank "1.4.3"]]
+  :jar-exclusions [#"aws.properties"])
   
