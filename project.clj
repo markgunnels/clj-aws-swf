8da6af5f0cc2104614a59ec27cf505085db120e5
Mark Gunnels <mark@mark-Lemur-Ultra.(none)>
2012-04-26 13:04:29 -0400

    temporary fix for workflow timeouts

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-aws-swf "1.0.6-SNAPSHOT"
+(defproject clj-aws-swf "1.0.7-SNAPSHOT"
   :description "Clojure library to make use of Amazon's Simple Workflow Framework"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [com.amazonaws/aws-java-sdk "1.3.3"]]
