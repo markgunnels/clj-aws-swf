de7b9de5fa6fbd7696e6a461a71b1bf2dbdf35bb
Mark Gunnels <mark@mark-Lemur-Ultra.(none)>
2012-05-29 07:50:57 -0400

    version bump on amazon sdk

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject clj-aws-swf "1.0.7-SNAPSHOT"
+(defproject clj-aws-swf "1.0.8-SNAPSHOT"
   :description "Clojure library to make use of Amazon's Simple Workflow Framework"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [com.amazonaws/aws-java-sdk "1.3.3"]]
+                 [com.amazonaws/aws-java-sdk "1.3.10"]]
   :plugins [[lein-swank "1.4.3"]]
   :jar-exclusions [#"aws.properties"])
   
