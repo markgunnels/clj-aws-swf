8eaf3d21f78d6cd217459e7bafb396ffae6925e5
Mark Gunnels <mark@mark-Lemur-Ultra.(none)>
2012-09-29 13:46:42 -0400

    version fix

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clj-aws-swf/clj-aws-swf "1.0.16-SNAPSHOT"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [com.amazonaws/aws-java-sdk "1.3.10"]
+                 [com.amazonaws/aws-java-sdk "1.3.21.1"]
                  [clj-time "0.4.4"]]
   :plugins [[lein-swank "1.4.3"]]
   :description "Clojure library to make use of Amazon's Simple Workflow Framework")
