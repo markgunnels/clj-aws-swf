ceeae0da7a4c147eb24ab63953d2a0b78a09062c
Mark Gunnels <mark@mark-Lemur-Ultra.(none)>
2012-09-29 13:53:03 -0400

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-aws-swf/clj-aws-swf "1.0.16-SNAPSHOT"
+(defproject clj-aws-swf/clj-aws-swf "1.0.17-SNAPSHOT"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [com.amazonaws/aws-java-sdk "1.3.21.1"]
