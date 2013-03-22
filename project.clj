09fb5b295723a32ece16dbd91cae6cab18dd808f
Ubuntu <ubuntu@domU-12-31-39-0E-80-8D.compute-1.internal>
2013-03-22 16:50:32 +0000

    version bumps

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject clj-aws-swf/clj-aws-swf "1.0.31-SNAPSHOT"
+(defproject clj-aws-swf/clj-aws-swf "1.0.32-SNAPSHOT"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/core.contracts "0.0.1"]                 
-                 [com.amazonaws/aws-java-sdk "1.3.21.1"]
+                 [com.amazonaws/aws-java-sdk "1.4.0.1"]
                  [clj-time "0.4.4"]
                  [inflections "0.7.3"]
                  [midje "1.4.0"]]
